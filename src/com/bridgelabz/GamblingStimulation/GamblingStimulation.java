99b3114769fe6ca691746c7c6e84c071494d9c1a
ShreyasNandish <shreyasnandish4@gmail.com>
2022-07-12 14:51:46 +0530

    Modified:modified the code to resign upto 50% for the day

--- src/com/bridgelabz/GamblingStimulation/GamblingStimulation.java
+++ src/com/bridgelabz/GamblingStimulation/GamblingStimulation.java
@@ -4,18 +4,22 @@ public class GamblingStimulation {
     static int dailyStake = 100;
     static int betInDollar = 1;
     public static final int win = 1;
+
     public static void main(String[] args) {
         System.out.println("Welcome To Gambling Simulator");
-        int result = (int) ((Math.random() * 10) % 2);
-        if(result == win) {
-            dailyStake++;
-            System.out.println("Won");
-        }else {
-            dailyStake--;
-            System.out.println("Loose");
+
+        while (dailyStake > 50 && dailyStake != 150) {
+            int result = (int) ((Math.random() * 10) % 2);
+            if (result == win) {
+                dailyStake++;
+                System.out.println("Won");
+            } else {
+                dailyStake--;
+                System.out.println("Loose");
+            }
+            System.out.println("Total daily Stake is : " + dailyStake);
+            System.out.println("Betting Dollar for every game is : " + betInDollar);
         }
-        System.out.println("Total daily Stake is : " + dailyStake);
-        System.out.println("Betting Dollar for every game is : " + betInDollar);
     }
 }
 
