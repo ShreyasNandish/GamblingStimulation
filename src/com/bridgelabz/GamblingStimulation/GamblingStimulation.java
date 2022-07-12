56729d7270df461ad149017b49c356846273c262
ShreyasNandish <shreyasnandish4@gmail.com>
2022-07-12 13:44:18 +0530

    Added: added stimulator program

--- src/com/bridgelabz/GamblingStimulation/GamblingStimulation.java
+++ src/com/bridgelabz/GamblingStimulation/GamblingStimulation.java
@@ -1,4 +1,8 @@
 package com.bridgelabz.GamblingStimulation;
 
 public class GamblingStimulation {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Gambling Simulator");
+    }
 }
+
